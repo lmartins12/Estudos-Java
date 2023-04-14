d43781ebbe765eac01cf18189a89485022b36d21
Lucas Martins <lucasml@ciandt.com>
2023-04-14 12:14:45 -0300

    Membros estáticos 2

--- VolumeCircumference/src/application/Main.java
+++ VolumeCircumference/src/application/Main.java
@@ -11,15 +11,13 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Calculator calc = new Calculator();
-
         System.out.print("Enter radius: ");
         double radius = sc.nextDouble();
-        double c = calc.circumference(radius);
-        double v = calc.volume(radius);
+        double c = Calculator.circumference(radius);
+        double v = Calculator.volume(radius);
 
         System.out.printf("Circumference: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("PI value: %.2f%n", calc.PI);
+        System.out.printf("PI value: %.2f%n", Calculator.PI);
     }
 }
